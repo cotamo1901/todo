4816fd40ae323907aa22a9279dc960fdcde30447
john Cotamo <johncotamo@hotmail.com>
2023-01-29 05:29:42 -0500

    first upload

---
+++ src/components/TodoCounter.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './TodoCounter.css';
+
+function TodoCounter() {
+    return (
+        <h2 className="TodoCounter">this complet 2 of 3 tod'os</h2>
+    );
+}
+
+export { TodoCounter };
